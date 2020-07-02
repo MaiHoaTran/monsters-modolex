53259eeb93913a6735d6862f2974179ea8ca62d2
htmtran <hoa.thi.mai.tran@mgm-tp.com>
2020-07-02 16:08:20 +0700

    Init project monsters molodex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./component/card-list/card-list.component";
+import { SearchBox } from "./component/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFiled: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((monsters) => this.setState({ monsters: monsters }));
+  }
+
+  handleChanged = (e) => {
+    this.setState({ searchFiled: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchFiled } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    );
+    return (
+      <div className="App">
+      <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChanged={this.handleChanged}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleChanged }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChanged}
+  />
+);
