ad2c56aa570fd15de2dbb9a1705d35d1531cc57d
htmtran <hoa.thi.mai.tran@mgm-tp.com>
2020-07-02 16:41:43 +0700

    Updates

---
+++ precache-manifest.29d13fcd5d0755e1e946a1689165fadd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8002f6a06e5f0eac2fb1519c52855ed0",
+    "url": "/monsters-molodex/index.html"
+  },
+  {
+    "revision": "57791f9bfc121da2d481",
+    "url": "/monsters-molodex/static/css/main.7297a4ce.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-molodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-molodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57791f9bfc121da2d481",
+    "url": "/monsters-molodex/static/js/main.0b77e2e6.chunk.js"
+  },
+  {
+    "revision": "95046d10decd952ca1fc",
+    "url": "/monsters-molodex/static/js/runtime-main.23ad7d52.js"
+  }
+]);
